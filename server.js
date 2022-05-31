9a80e203a7ba3ec3bc7e3288ac6d54794671e467
Henry Lee - Dev <henrylee.contact@gmail.com>
2022-05-31 13:41:39 -0400

    changed database

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 // why is mongo uppercase?
-const url = 'mongodb+srv://simple-app:app123@cluster0.wa9nr.mongodb.net/?retryWrites=true&w=majority'
+const url = 'mongodb+srv://supermarket-list:list123@supermarket-express.vywtz.mongodb.net/supermarketExpressList?retryWrites=true&w=majority'
 
 app.listen(2000, function() {
   console.log('listening on 2000')
@@ -24,8 +24,8 @@ app.listen(2000, function() {
 MongoClient.connect(url, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-    const quotesCollection = db.collection('quotes')
+    const db = client.db('supermarketExpressList')
+    const expressListCollection = db.collection('expressList')
 
     app.set('view engine', 'ejs')
     app.use(bodyParser.urlencoded({ extended: true }))
@@ -34,16 +34,16 @@ MongoClient.connect(url, { useUnifiedTopology: true })
 
     //how it's grabbing the list of quotes from the database
   app.get('/', (req, res) => {
-    db.collection('quotes').find().toArray()
+    db.collection('expressList').find().toArray()
     .then(results => {
-        res.render('index.ejs', { quotes: results })
+        res.render('index.ejs', { expressList: results })
       })
       .catch(error => console.error(error))
   })
 
     //This inserts the added quote from the form, into the (this collection, insert one from req.body of the form request)
   app.post('/quotes', (req, res) => {
-    quotesCollection.insertOne(req.body)
+    expressListCollection.insertOne(req.body)
       .then(result => {
         res.redirect('/')
       })
@@ -51,7 +51,7 @@ MongoClient.connect(url, { useUnifiedTopology: true })
   })
 
   app.put('/replace', (req, res) => {
-    quotesCollection.findOneAndUpdate(
+    expressListCollection.findOneAndUpdate(
       { item: req.body.item },
       {
         $set: {
@@ -68,7 +68,7 @@ MongoClient.connect(url, { useUnifiedTopology: true })
   })
 
   app.delete('/quotes', (req, res) => {
-    quotesCollection.deleteOne(
+    expressListCollection.deleteOne(
       { name: req.body.name }
     )
     .then(result => {
@@ -81,7 +81,7 @@ MongoClient.connect(url, { useUnifiedTopology: true })
   })
 
   // app.delete('/quotes', (req, res) => {
-  //   quotesCollection.findOneAndDelete(
+  //   expressListCollection.findOneAndDelete(
   //     { item: req.body.item,
 
   //       name: req.body.name, 
@@ -99,7 +99,7 @@ MongoClient.connect(url, { useUnifiedTopology: true })
 
 
   app.delete('/itemsAll', (req, res) => {
-    quotesCollection.deleteMany(
+    expressListCollection.deleteMany(
       // when deleting all the data in the collection, you just have to leave the query blank..
     )
     .then(result => {
